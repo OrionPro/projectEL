9546d047b49ea72edafeb404f4a9a28ae3b7a7b6
orion <orionpro>
2016-11-24 15:06:27 +0200

    мой ком

--- app/js/1.js
+++ app/js/1.js
@@ -1,6 +1,4 @@
-Моя 
-Версия
-Проверка
-добавил
-ещё добавил
-235235
+tl7.from("#path4402", 1, { opacity: 0, ease: Power2.easeInOut}, 0.8); //
+tl7.from("#path4476", 1, { opacity: 0, ease: Power2.easeInOut}, 1.2);
+tl7.from("#path4562", 1, { opacity: 0, ease: Power2.easeInOut}, 1.7);
+    

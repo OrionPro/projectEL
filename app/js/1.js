1cf311ce6bcac3cf2adfcf7559923bbb47994fd9
OrionPro <orion79@rambler.ru>
2016-11-24 15:06:11 +0200

    Update 1.js

--- app/js/1.js
+++ app/js/1.js
@@ -1,6 +1,3 @@
-Моя 
-Версия
-Проверка
-добавил
-ещё добавил
-235235
+tl7.from("#path4341", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0);
+tl7.from("#g4912", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.3);
+tl7.from("#text4263-3", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.6);

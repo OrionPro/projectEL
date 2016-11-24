dc186cfb2e11c3f94b9c5a22cf52563611c904ef
orion <orionpro>
2016-11-24 15:08:08 +0200

    124

--- app/js/1.js
+++ app/js/1.js
@@ -1,4 +1,4 @@
-tl7.from("#path4402", 1, { opacity: 0, ease: Power2.easeInOut}, 0.8); //
+tl7.from("#path4402", 1, { opacity: 0, ease: Power2.easeInOut}, 0.7); //
 tl7.from("#path4476", 1, { opacity: 0, ease: Power2.easeInOut}, 1.2);
-tl7.from("#path4562", 1, { opacity: 0, ease: Power2.easeInOut}, 1.7);
+tl7.from("#path4562", 1, { opacity: 0, ease: Power2.easeInOut}, 1.1);
     

324fc347851e81ae2280be7a5d7475cae6904217
orion <orionpro>
2016-11-24 15:20:58 +0200

    3525

--- app/js/1.js
+++ app/js/1.js
@@ -1,17 +1,16 @@
-tl7.from("#path4402", 1.2, { opacity: 0, ease: Power2.easeInOut}, 0.7); 
-tl7.from("#path4476", 1.2, { opacity: 0, ease: Power2.easeInOut}, 1);
-tl7.from("#path4562", 1.2, { opacity: 0, ease: Power2.easeInOut}, 1);
-    
-tl7.from("#path4341", 1.2, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0);
-tl7.from("#g4912", 1.2, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.3);
-tl7.from("#text4263-3", 1.2, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.6);
+ tl7.from("#g5164_item1", 1, { rotation: 360, transformOrigin: "50% 50%", opacity: 0, x: -100, y: -100, ease: Power2.easeInOut}, 0);
+    tl7.from("#g5164_item2", 1, { rotation: 360, transformOrigin: "50% 50%", opacity: 0, x: -100, y: -100, ease: Power2.easeInOut}, 0.3);
+    tl7.from("#g5164_item3", 1, { rotation: 360, transformOrigin: "50% 50%", opacity: 0, x: -100, y: -100,  ease: Power2.easeInOut}, 0.6);
+    tl7.from("#g5164_item4", 1, { rotation: 360, transformOrigin: "50% 50%", opacity: 0, x: -100, y: -100,  ease: Power2.easeInOut}, 0.9);
+    // внутренние иконки
+    tl7.from("#path4341", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0);
+    tl7.from("#g4912", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.3);
+    tl7.from("#text4263-3", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.6);
+    tl7.from("#text4263-223", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.9);
 
-tl7.from("#path4402", 1, { opacity: 0, ease: Power2.easeInOut}, 0); 
-tl7.from("#path4476", 1, { opacity: 0, ease: Power2.easeInOut}, 1.1);
-tl7.from("#path4562", 1, { opacity: 0, ease: Power2.easeInOut}, 1.1);
-    
-tl7.from("#path4341", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.1);
-tl7.from("#g4912", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.1);
-tl7.from("#text4263-3", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.1);
+    tl7.from("#path4402", 1, { opacity: 0, ease: Power2.easeInOut}, 0.8); //
+    tl7.from("#path4476", 1, { opacity: 0, ease: Power2.easeInOut}, 1.2);
+    tl7.from("#path4562", 1, { opacity: 0, ease: Power2.easeInOut}, 1.7);
+    tl7.from("#path4669", 1, { opacity: 0, ease: Power2.easeInOut}, 2);
 
 

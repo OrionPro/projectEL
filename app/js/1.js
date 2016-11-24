dcdc679cebe9d9cbc262637395dcb2a3911edbd5
OrionPro <orion79@rambler.ru>
2016-11-24 15:07:50 +0200

    Update 1.js

--- app/js/1.js
+++ app/js/1.js
@@ -1,4 +1,4 @@
-tl7.from("#path4402", 1, { opacity: 0, ease: Power2.easeInOut}, 0.8); //
-tl7.from("#path4476", 1, { opacity: 0, ease: Power2.easeInOut}, 1.2);
-tl7.from("#path4562", 1, { opacity: 0, ease: Power2.easeInOut}, 1.7);
+tl7.from("#path4341", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0);
+tl7.from("#g4912", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.3);
+tl7.from("#text4263-3", 1, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.6);
     

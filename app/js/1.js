78277123c02979224a0799ef65584a437ef74f5e
OrionPro <orion79@rambler.ru>
2016-11-24 15:28:59 +0200

    Update 1.js

--- app/js/1.js
+++ app/js/1.js
@@ -1,10 +1,11 @@
-tl7.from("#path4402", 1.2, { opacity: 0, ease: Power2.easeInOut}, 0.7); 
-tl7.from("#path4476", 1.2, { opacity: 0, ease: Power2.easeInOut}, 1);
-tl7.from("#path4562", 1.2, { opacity: 0, ease: Power2.easeInOut}, 1);
-    
-tl7.from("#path4341", 1.2, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0);
-tl7.from("#g4912", 1.2, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.3);
-tl7.from("#text4263-3", 1.2, { opacity: 0, x: -150, y: 0,  ease: Power2.easeInOut}, 0.6);
+//  ковбой
+    tl2.from("#g4315", 0.6, { opacity: 0, x: 80, ease: Power2.easeInOut }, 0.1);
+    tl2.from("#rect4448", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
+    tl2.from("#rect4450", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
+    tl2.from("#g4321", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
+    tl2.from("#g4510", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
+    tl2.from("#rect4443", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
+    tl2.from("#g4479", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut, }, 0.4);
 
 
 

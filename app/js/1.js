281a8a8826bd4b9c9acf7f20eae841afcdae19a6
OrionPro <orion79@rambler.ru>
2016-11-24 15:33:18 +0200

    Update 1.js

--- app/js/1.js
+++ app/js/1.js
@@ -10,8 +10,9 @@
         function() {
             TweenMax.to($(this).find(".path4213-item"), 0.7, { fill: "#177898" });
             TweenMax.to($(this).find(".path4278"), 0.7, { fill: "#0f7289" });
-            TweenMax.to($(this).find(".path4393"), 0.7, { fill: "#0f4e5d" });
             TweenMax.to($(this).find("p"), 0.7, { color: "#fff" });
+            TweenMax.to($(this).find(".path4393"), 0.7, { fill: "#0f4e5d" });
+           
         }
     );
 
@@ -19,8 +20,8 @@
     tl2.from("#g4315", 0.6, { opacity: 0, x: 80, ease: Power2.easeInOut }, 0.1);
     tl2.from("#rect4448", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
     tl2.from("#rect4450", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
-    tl2.from("#g4321", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
-    tl2.from("#g4510", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
+     tl2.from("#g4510", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
+    tl2.from("#g4321", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);   
     tl2.from("#rect4443", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut }, 0.4);
     tl2.from("#g4479", 0.5, { opacity: 0, y: 80, ease: Power2.easeInOut, }, 0.4);
 
